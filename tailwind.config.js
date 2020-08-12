57d54d465a977bd46186a0cf6d0320db6a6f050f
matevosyanmko <matevosyanmko@gmail.com>
2020-08-13 02:13:37 +0400

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@ module.exports = {
   purge: ["./shared/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    height: {
-      "90px": "90px",
-      "110px": "110px",
-      "200px": "200px",
-      "400px": "400px",
-    },
     screens: {
       xs: "0px",
       sm: "640px",
@@ -16,6 +10,12 @@ module.exports = {
       xl: "1280px",
     },
     extend: {
+      height: {
+        "90px": "90px",
+        "110px": "110px",
+        "200px": "200px",
+        "400px": "400px",
+      },
       width: {
         "300px": "300px",
       },
