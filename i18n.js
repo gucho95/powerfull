918f8392675b2eed67bedb0491657268f6f6896a
matevosyanmko <matevosyanmko@gmail.com>
2020-08-22 01:04:52 +0400

    fixes in i18n

--- i18n.js
+++ i18n.js
@@ -5,6 +5,7 @@ module.exports = new NextI18Next({
   defaultLanguage: "hy",
   otherLanguages: ["en", "ru"],
   localeSubpaths: {
+    hy: "hy",
     ru: "ru",
     en: "en",
   },
---
+++ pages/user_agreement_and_privacy_policy.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { withTranslation } from "i18n";
+
+const Privacy = ({ t, i18n }) => {
+  return (
+    <div className='bg-white'>
+      <Head>
+        <title>Powerfull | Homepage</title>
+      </Head>
+      privacy
+    </div>
+  );
+};
+
+Privacy.getInitialProps = async () => ({
+  namespacesRequired: ["common", "sections", "footer"],
+});
+
+export default withTranslation("common")(Privacy);
